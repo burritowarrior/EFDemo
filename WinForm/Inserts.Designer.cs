a66893942ec99f60477064f4d7d214d3ce777302
chris <coson16@gmail.com>
2013-06-11 13:07:38 -0700

    Adding Inserts - Includes stored procedures

---
+++ WinForm/Inserts.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WinForm
+{
+    partial class Inserts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(96, 30);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFirstName.TabIndex = 0;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(96, 56);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLastName.TabIndex = 1;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(96, 82);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddress.TabIndex = 2;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(96, 124);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 3;
+            this.buttonSubmit.Text = "button1";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Inserts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Name = "Inserts";
+            this.Text = "Inserts";
+            this.Load += new System.EventHandler(this.Inserts_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WinForm/Inserts.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WinForm
+{
+    public partial class Inserts : Form
+    {
+        private const string STR_CONNECTION = @"server=localhost;User Id=sqluser;Password=sqluser;database=development";
+
+        public Inserts()
+        {
+            InitializeComponent();
+        }
+
+        private void Inserts_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void buttonSubmit_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                using (var sqlConnection = new MySqlConnection(STR_CONNECTION))
+                using (var sqlCommand = new MySqlCommand("InsertEmployee", sqlConnection))
+                {
+                    sqlConnection.Open();
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.Add("firstName", MySqlDbType.VarChar, 15).Value = textBoxFirstName.Text.Trim();
+                    sqlCommand.Parameters.Add("lastName", MySqlDbType.VarChar, 15).Value = textBoxLastName.Text.Trim();
+                    sqlCommand.Parameters.Add("address", MySqlDbType.VarChar, 64).Value = textBoxAddress.Text.Trim();
+
+                    sqlCommand.ExecuteNonQuery();
+
+                    label1.Text = @"Success!";
+                }
+            }
+            catch (MySqlException mse)
+            {
+                label1.Text = mse.Message;
+            }
+
+        }
+    }
+}
